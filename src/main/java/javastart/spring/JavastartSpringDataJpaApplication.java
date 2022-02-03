1ce69ececd34b7d550089e8c682c91070dfadd92
Krzysztof <krzubrycki@gmail.com>
2022-02-03 16:32:48 +0100

    added test bike to database

--- src/main/java/javastart/spring/JavastartSpringDataJpaApplication.java
+++ src/main/java/javastart/spring/JavastartSpringDataJpaApplication.java
@@ -2,12 +2,17 @@ package javastart.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class JavastartSpringDataJpaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(JavastartSpringDataJpaApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(JavastartSpringDataJpaApplication.class, args);
+        Bike bike1= new Bike(1L,"Kross Esker 4.0, 29 cali męski", "KRS12345", 30, 100);
+        BikeRepository bikeRepository = context.getBean(BikeRepository.class);
+        bikeRepository.save(bike1);
+        System.out.println("Zapisano w bazie bike1");
     }
 
 }
