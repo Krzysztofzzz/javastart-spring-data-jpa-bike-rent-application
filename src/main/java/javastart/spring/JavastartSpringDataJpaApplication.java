4a222951f95bf525e3a3dad8309b1796e9f420a8
Krzysztof <krzubrycki@gmail.com>
2022-02-03 16:18:23 +0100

    created project and added properties to run it in h2-console

---
+++ src/main/java/javastart/spring/JavastartSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package javastart.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavastartSpringDataJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavastartSpringDataJpaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/javastart/spring/JavastartSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package javastart.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavastartSpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
