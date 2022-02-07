ef5281ed8135d59ab835bdf05a5fadf19e0fa40e
Krzysztof <krzubrycki@gmail.com>
2022-02-07 17:01:43 +0100

    testing rentForHours method

--- src/main/java/javastart/spring/JavastartSpringDataJpaApplication.java
+++ src/main/java/javastart/spring/JavastartSpringDataJpaApplication.java
@@ -8,12 +8,13 @@ import org.springframework.context.ConfigurableApplicationContext;
 public class JavastartSpringDataJpaApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(JavastartSpringDataJpaApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(JavastartSpringDataJpaApplication    .class, args);
         NewBikeDto bike1 = new NewBikeDto(1L, "Kross Esker 4.0, 29 cali męski", "KRS12345", 30, 100);
         BikeService bikeService = context.getBean(BikeService.class);
         bikeService.add(bike1);
-        bike1 = new NewBikeDto(1L, "Kross Esker 4.0, 29 cali męski", "KRS12345", 50, 100); //duplikat id
-        bikeService.add(bike1);
+        double payment = bikeService.rentForHours(1L, 5, "ABC999");
+        System.out.println("Należność do zapłaty za wypożyczenie: " + payment);
+        bikeService.returnBike(1L);
     }
 
 }
