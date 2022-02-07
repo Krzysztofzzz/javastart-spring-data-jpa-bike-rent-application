007c9eb9c7afd972560546a78b509f377e44c4e4
Krzysztof <krzubrycki@gmail.com>
2022-02-07 17:23:13 +0100

    extended BikeRepository with CrudRepository

--- src/main/java/javastart/spring/BikeRepository.java
+++ src/main/java/javastart/spring/BikeRepository.java
@@ -1,37 +1,6 @@
 package javastart.spring;
 
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.repository.CrudRepository;
 
-import javax.persistence.EntityManager;
-import java.util.Optional;
-
-@Repository
-public class BikeRepository {
-    private final EntityManager entityManager;
-
-    public BikeRepository(EntityManager entityManager) {
-        this.entityManager = entityManager;
-    }
-
-    public void save(Bike bike) {
-        if (exists(bike)) {
-            entityManager.merge(bike);
-        } else {
-            entityManager.persist(bike);
-        }
-    }
-
-    public Optional<Bike> findById(Long id) {
-        return Optional.ofNullable(entityManager.find(Bike.class, id));
-    }
-
-    public void deleteById(Long id) {
-        findById(id).ifPresent(entityManager::remove);
-    }
-
-    private boolean exists(Bike bike) {
-        return entityManager.find(Bike.class, bike.getId()) != null;
-    }
-
+interface BikeRepository extends CrudRepository<Bike, Long> {
 }
